09bbe0fe4794379f28628816ba4f82db06f43d4c
Stephen Bussey <sb8244@cs.ship.edu>
2014-04-23 12:41:55 -0400

    Pull in code mirror for submission page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require codemirror
+//= require codemirror/modes/clike
 //= require_tree .
 
 $(document).ready(function() {
